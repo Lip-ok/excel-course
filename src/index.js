8894f1b70b3b7138b8edeef38e902ddb7a407640
Alexander Lipen <lipenjob@gmail.com>
2020-10-20 21:31:22 +0300

    Finish framework functional

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './scss/index.scss'
-console.log('Hello World!!!')
+import {Excel} from '@/components/excel/Excel';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Header} from '@/components/header/Header';
+import {Table} from '@/components/table/Table';
+import {Formula} from '@/components/formula/Formula';
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
