e669ef7622ed08a5b8b10340793646aded676680
Alexander Lipen <lipenjob@gmail.com>
2020-11-12 16:33:39 +0300

    Finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,18 +5,40 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name
+    this.emitter = options.emitter
+    this.unsubscribers = []
+
+    this.prepare()
   }
+  // Настраиваем наш компонент до init
+  prepare() {}
 
   // Return pattern component
   toHTML() {
     return ''
   }
 
+  // Инициализируем компонент
+  // Добавляем DOM слушателей
   init() {
     this.initDOMListener()
   }
 
+  // Уведомляем слушателя про event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // Подписываемся на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
+  // Удаляем компонент
+  // Чистим слушателей
   destroy() {
     this.removeDOMListaners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -49,6 +49,21 @@ class Dom {
     return this.$el.getBoundingClientRect()
   }
 
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text
+      return this
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim()
+  }
+
+  find(selector) {
+    return $(this.$el.querySelector(selector))
+  }
+
   findAll(selector) {
     return this.$el.querySelectorAll(selector)
   }
@@ -59,6 +74,32 @@ class Dom {
         .forEach(key => {
           this.$el.style[key] = styles[key]
         })
+  }
+
+  focus() {
+    this.$el.focus()
+    return this
+  }
+
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':')
+      return {
+        row: +parsed[0],
+        col: +parsed[1]
+      }
+    }
+    return this.data.id
+  }
+
+  addClass(className) {
+    this.$el.classList.add(className)
+    return this
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className)
+    return this
   }
 }
 
